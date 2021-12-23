2a96da9985f268d5247f815f55d6790d1fa008ea
papa-react <makiya112545@gmail.com>
2021-12-23 08:49:22 -1200

    add glass-moriphisem effects

--- components/card.jsx
+++ components/card.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image"
 import Link from "next/link"
 
-const Card = ({img,title,}) => {
+const Card = ({img,title,id}) => {
     return (
-        <Link  href="/coffee-store/ioeio" passHref>
-            <div className="cursor-pointer w-[300px] h-[210px] md:w-[320px] md:h-[230px] flex flex-col items-center justify-center pb-2 rounded-xl bg-slate-50">
-                <h3 className=" w-full flex py-1 text-base  font-semibold justify-start pl-5 truncate  ">coffee app for all </h3>
-                <div className="relative w-[260px]  h-[150px] md:w-[280px] md:h-[170px] bg-slate-50  " >
+        <Link  href={`/coffee-store/${id}`} passHref>
+            <div className=" bg-opacity-20 hover:bg-opacity-40  bg-clip-padding backdrop-filter backdrop-blur-sm cursor-pointer w-[300px] h-[210px] md:w-[320px] md:h-[230px] flex flex-col items-center justify-center pb-2 rounded-xl bg-gray-200  ">
+                <h3 className=" w-full flex py-1 text-base  font-semibold justify-start pl-5 truncate text-white ">{title}</h3>
+                <div className="relative rounded-xl w-[260px]  h-[150px] md:w-[280px] md:h-[170px] bg-slate-50  " >
                     <Image className="rounded-xl" objectFit="cover" layout="fill" src={img} alt={title}/>
                 </div>
             </div>
