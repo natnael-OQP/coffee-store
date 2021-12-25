d8867698e9ef81a342f12af171a60895383de43f
papa-react <makiya112545@gmail.com>
2021-12-25 02:13:35 -1200

    add unsplash_key

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ module.exports = {
   images: {
     domains: ['images.unsplash.com'],
     formats: ['image/avif', 'image/webp'],
+  },
+  env: {
+    unsplash_key:'dLx38PktEnNSYB3oFEX6i9q0URMCkbp4lW8R2eht73E'
   }
 }
